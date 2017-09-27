129a5fce36e8d7ebab45ac4b525fd9cbe366abfb
BenJilks <benjyjilks@gmail.com>
2017-09-27 09:14:58 +0000

    Update

--- include/Colour.h
+++ include/Colour.h
@@ -4,26 +4,28 @@
 #include "Vector.h"
 
 /* Runtime colours */
-static Vec3f White       = {1,1,1};
-static Vec3f Black       = {0,0,0};
+static Vec3f White       = {1.00, 1.00, 1.00};
+static Vec3f Black       = {0.00, 0.00, 0.00};
 static Vec3f DarkGrey    = {0.73, 0.68, 0.63};
-static Vec3f Grey        = {0.8, 0.75, 0.7};
+static Vec3f Grey        = {0.80, 0.75, 0.70};
 static Vec3f LightGrey   = {0.93, 0.89, 0.85};
 static Vec3f Four        = {0.93, 0.88, 0.78};
 static Vec3f LightOrange = {0.95, 0.69, 0.47};
 static Vec3f Orange      = {0.96, 0.49, 0.37};
 static Vec3f DarkOrange  = {0.96, 0.37, 0.23};
+static Vec3f Yellow      = {0.93, 0.80, 0.38}; 
 static Vec3f FirstNumber = {0.44, 0.40, 0.37};
 
 /* Compile time colours */
-#define WHILE        {1,1,1}
-#define BLACK        {0,0,0}
+#define WHILE        {1.00, 1.00, 1.00}
+#define BLACK        {0.00, 0.00, 0.00}
 #define DARK_GRAY    {0.73, 0.68, 0.63}
-#define GREY         {0.8, 0.75, 0.7}
+#define GREY         {0.80, 0.75, 0.70}
 #define LIGHT_GRAY   {0.93, 0.89, 0.85}
 #define FOUR         {0.93, 0.88, 0.78}
 #define LIGHT_ORANGE {0.95, 0.69, 0.47}
 #define ORANGE       {0.96, 0.49, 0.37}
+#define YELLOW       {0.93, 0.80, 0.38}
 #define DARK_ORANGE  {0.96, 0.37, 0.23}
 
 #endif // COLOUR_H
--- src/Board.c
+++ src/Board.c
@@ -23,7 +23,7 @@ Board Bd_NewBoard()
 	srand(time(0));
 	for (i = 0; i < SIZE; i++)
 	{
-		board.tiles[i] = rand() % 6;
+		board.tiles[i] = rand() % 10;
 		/*if (rand() % 4 == 0)
 		{
 			board.tiles[i] = 1;
@@ -52,7 +52,7 @@ void DrawTile(int index, int value)
 	int y = (int)(index / ROWS) * stepY + BOARDER;
 	Vec2i start = {x, y};
 	Vec2i size = {tileWidth, tileHeight};
-	WM_DrawRect(start, size, colours[value]);
+	WM_DrawRect(start, size, value <= 5 ? colours[value] : Yellow);
 	
 	if (value > 0)
 	{
@@ -60,7 +60,8 @@ void DrawTile(int index, int value)
 		char numberStr[80];
 		sprintf(numberStr, "%i", (int)pow(2, value));
 		Vec2i textPosition = {x + (tileWidth / 2.0f), y + (tileHeight / 2.0f)};
-		WM_DrawText(numberStr, textPosition, "Testing.ttf", 60, value <= 2 ? FirstNumber : White);
+		WM_DrawText(numberStr, textPosition, "Testing.ttf", 60, 
+			value <= 2 ? FirstNumber : White);
 	}
 }
 
@@ -73,3 +74,27 @@ void Bd_DrawBoard(Board board)
 		DrawTile(i, board.tiles[i]);
 }
 
+void ScanTiles(Board* board, int from, int step, int dir)
+{
+	int x, y;
+	for (x = 0; x < ROWS; x++)
+	{
+		for (y = from; y < COLUMNS && y >= 0; y += step)
+		{
+			ProcessTile(board, x, y);
+		}
+	}
+}
+
+void ScanColumns(Board* board, int from, int step)
+{
+	int x, y;
+	for (y = 0; y < COLUMNS; y++)
+		for (x = from; x < ROWS && x >= 0; x += step)
+			ProcessTile(board, x, y);
+}
+
+void Bd_Move(Board* board, int dir)
+{
+
+}
